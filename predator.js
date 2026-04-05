f885a2d6471669592ff9ddbe420ecfc18d3bfc94
Creamiiii <stillnothing4now@gmail.com>
2026-04-05 17:52:21 +0800

    Update predator.js

--- predator.js
+++ predator.js
@@ -56,6 +56,9 @@ class Predator {
     vertex(-8 * this.size, 4 * this.size);
     endShape();
     pop();
+
+    fill(255);
+    ellipse(this.pos.x, this.pos.y, this.perception * 2, this.perception * 2);
   }
 
   wander() {
